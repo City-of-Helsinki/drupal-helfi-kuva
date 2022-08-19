2c0499e6eaf117839832a2b465d85a373eb9c788
Risto-Pekka Nykänen <risto.nykanen@druid.fi>
2022-08-19 12:17:24 +0300

    revert deleted files

---
+++ public/sites/default/development.settings.php
@@ -0,0 +1,9 @@
+<?php
+
+$config['helfi_proxy.settings']['asset_path'] = 'dev-kuva-assets';
+$config['helfi_proxy.settings']['prefixes'] = [
+  'en' => 'dev-culture-and-leisure',
+  'fi' => 'dev-kulttuuri-ja-vapaa-aika',
+  'sv' => 'dev-kultur-och-fritid',
+  'ru' => 'dev-culture-and-leisure',
+];
---
+++ public/sites/default/testing.settings.php
@@ -0,0 +1,10 @@
+<?php
+
+// These should be unique for each environment (dev-, test-, staging-)
+$config['helfi_proxy.settings']['asset_path'] = 'test-kuva-assets';
+$config['helfi_proxy.settings']['prefixes'] = [
+  'en' => 'test-culture-and-leisure',
+  'fi' => 'test-kulttuuri-ja-vapaa-aika',
+  'sv' => 'test-kultur-och-fritid',
+  'ru' => 'test-culture-and-leisure',
+];
