52b36f489689f1afdc16075568f4e4ea0f85b5a0
tuutti <tuutti@iki.fi>
2024-02-13 11:10:56 +0200

    UHF-9546: Removed unnecessary settings

--- public/sites/default/testing.settings.php
+++ public/sites/default/testing.settings.php
@@ -1,14 +1,6 @@
 <?php
 
 $config['helfi_proxy.settings']['tunnistamo_return_url'] = '/fi/kulttuuri-ja-vapaa-aika/openid-connect/tunnistamo';
-$config['helfi_proxy.settings']['asset_path'] = 'kuva-assets';
-$config['helfi_proxy.settings']['prefixes'] = [
-  'en' => 'culture-and-leisure',
-  'fi' => 'kulttuuri-ja-vapaa-aika',
-  'sv' => 'kultur-och-fritid',
-  'ru' => 'culture-and-leisure',
-  'zxx' => 'culture-and-leisure',
-];
 
 $config['openid_connect.client.tunnistamo']['settings']['debug_log'] = TRUE;
 $config['openid_connect.client.tunnistamo']['settings']['ad_roles'] = [
